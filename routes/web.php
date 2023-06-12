94d28a7c0f85f2ee930e868561051e4ac683c17b
CptAs <arpek2121@gmail.com>
2023-06-12 20:39:22 +0200

    Added missing endpoints for all localll services

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\FirebaseController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+    // BASE ROUTES
 
     Route::get('get-firebase-data', [FirebaseController::class, 'auth'])->name('firebase.index');
 
@@ -23,14 +24,11 @@ use App\Http\Controllers\FirebaseController;
         return view('gate');
     })->name('home');
 
+
+    // MARKING LANDMARKS SERVICE
+
     Route::get('/getLandmark/{lat}/{lng}', function (Request $request) {
         return redirect()->away(Services::MARKING_LANDMARKS_URL . '/landmark/' . $request->lat . '/' . $request->lng);
-    });
-    Route::get('/getAllLandmarks', function () {
-        return redirect()->away(Services::MARKING_LANDMARKS_URL . '/getAllLandmarks');
-    });
-    Route::get('/getCommentsForLandmark/{landmarkId}', function (Request $request) {
-        return redirect()->away(Services::MARKING_LANDMARKS_URL . '/getCommentsForLandmark/' . $request->landmarkId);
     });
     Route::post('/updateProperty', function (Request $request) {
         return Http::post(Services::MARKING_LANDMARKS_URL . '/landmark/updateProperty', [
@@ -39,6 +37,18 @@ use App\Http\Controllers\FirebaseController;
             'idLandmark' => $request->idLandmark,
         ]);
     });
+    Route::get('/getAllLandmarks', function () {
+        return redirect()->away(Services::MARKING_LANDMARKS_URL . '/getAllLandmarks');
+    });
+    Route::get('/getAllLandmarksForUser/{id_user}', function (Request $request) {
+        return redirect()->away(Services::MARKING_LANDMARKS_URL . '/getAllLandmarksForUser/' . $request->id_user);
+    });
+    Route::delete('/deleteLandmark/{landmarkId}', function (Request $request) {
+        return redirect()->away(Services::MARKING_LANDMARKS_URL . '/deleteLandmark/' . $request->landmarkId);
+    });
+    Route::get('/getCommentsForLandmark/{landmarkId}', function (Request $request) {
+        return redirect()->away(Services::MARKING_LANDMARKS_URL . '/getCommentsForLandmark/' . $request->landmarkId);
+    });
     Route::post('/addComment', function (Request $request) {
         return Http::post(Services::MARKING_LANDMARKS_URL . '/landmark/addComment', [
             'content' => $request->content,
@@ -46,15 +56,37 @@ use App\Http\Controllers\FirebaseController;
         ]);
     });
 
+    // TOURS PLANNING SERVICE
+
     Route::get('/getAllTours', function () {
         return redirect()->away(Services::TOURS_PLANNING_URL . '/getAllTours');
+    });
+    Route::get('/getAllLandmarksForTour/{tour}', function (Request $request) {
+        return redirect()->away(Services::TOURS_PLANNING_URL . '/getAllLandmarksForTour/' . $request->tour);
     });
     Route::post('/storeTour', function (Request $request) {
         return Http::post(Services::TOURS_PLANNING_URL . '/storeTour', [
             'landmarksList' => $request->landmarksList,
         ]);
     });
+    Route::delete('/delete/{tour}', function (Request $request) {
+        return redirect()->away(Services::TOURS_PLANNING_URL . '/delete/' . $request->tour);
+    });
 
+    // GUIDED TOURS SERVICE
+
+    Route::get('/getReservation/{reservation}', function (Request $request) {
+        return redirect()->away(Services::GUIDED_TOURS_URL . '/getReservation/' . $request->reservation);
+    });
+    Route::get('/getAllTours', function () {
+        return redirect()->away(Services::GUIDED_TOURS_URL . '/getAllTours');
+    });
+    Route::get('/getAllLandmarksForTour/{tour}', function (Request $request) {
+        return redirect()->away(Services::GUIDED_TOURS_URL . '/getAllLandmarksForTour/' . $request->tour);
+    });
+    Route::get('/getAllReservations', function () {
+        return redirect()->away(Services::GUIDED_TOURS_URL . '/getAllReservations');
+    });
     Route::get('/getReservationsForUser/{user}', function () {
         return redirect()->away(Services::GUIDED_TOURS_URL . '/getReservationsForUser');
     });
@@ -63,6 +95,11 @@ use App\Http\Controllers\FirebaseController;
             'idUser' => $request->userId,
             'idTour' => $request->tourId,
             'date' => $request->tour_datetime
+        ]);
+    });
+    Route::post('/makePayment', function (Request $request) {
+        return Http::post(Services::GUIDED_TOURS_URL . '/makePayment', [
+             'reservation' => $request->reservationId
         ]);
     });
 // Route::post('/landmark/updateProperty', [LandmarkUserController::class, 'updateProperty']);
